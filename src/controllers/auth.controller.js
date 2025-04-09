8d1322d53918ba31dbde76b250f887ed5623998e
Sebastian Lopez <sebalopeza@uc.cl>
2025-04-09 14:17:39 -0400

    fix auth functional

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,13 +2,16 @@ const prisma = require('../prisma/client');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const register = async (req, res) => {
-  const { name, email, password, role } = req.body;
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
+const register = async (req, res) => {
   try {
+    const { name, email, password, role } = req.body;
+    if (!email || !password || !name ) throw new Error("Todos los campos son obligatorios.");
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.user.create({
+    const newUser = await prisma.user.create({
       data: {
         name,
         email,
@@ -17,29 +20,52 @@ const register = async (req, res) => {
       }
     });
 
-    res.status(201).json({ message: 'Usuario creado', userId: user.id });
+    const payloadSanitizedUser = {
+      id: newUser.id,
+      email: newUser.email,
+      name: newUser.name,
+      role: newUser.role,
+      updatedAt: newUser.updatedAt,
+      createdAt: newUser.createdAt,
+    };
+    const token = jwt.sign({ user: payloadSanitizedUser }, JWT_SECRET, { expiresIn: "12h" });
+
+    return res.json({ user: payloadSanitizedUser, token });
   } catch (error) {
-    res.status(400).json({ error: 'Error al registrar usuario', detail: error.message });
+    console.error("Error al intentar registrar un nuevo usuario ->", error.message);
+    return res.status(500).json({ message: error.message });
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-
   try {
+    const { email, password } = req.body;
+    if (!email || !password) throw new Error("El email y la contraseña son obligatorios.");
     const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) return res.status(401).json({ error: 'Credenciales inválidas' });
+    if (!user) throw new Error("El usuario con las credenciales señaladas no existe.");
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ error: 'Credenciales inválidas' });
+    if (!isMatch) throw new Error("El usuario con las credenciales señaladas no existe.");
 
-    const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
-      expiresIn: '1d'
-    });
+    const payloadSanitizedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      updatedAt: user.updatedAt,
+      createdAt: user.createdAt,
+    };
 
-    res.json({ token, user: { id: user.id, name: user.name, role: user.role } });
+    const token = jwt.sign({ user: payloadSanitizedUser }, JWT_SECRET, { expiresIn: "12h" });
+
+    return res.json({ user: payloadSanitizedUser, token });
   } catch (error) {
-    res.status(500).json({ error: 'Error al iniciar sesión', detail: error.message });
+    console.error("Error al intentar iniciar sesión ->", error.message);
+    return res.status(500).json({ message: error.message });
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
