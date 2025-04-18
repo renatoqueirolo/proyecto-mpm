e74df143ece7f92ae5cb4c01849391fe339427f6
Sebastian Lopez <sebalopeza@uc.cl>
2025-04-18 13:05:00 -0400

    fix after merge

--- src/routes/opt-model/plane/assignmentPlane.routes.js
+++ src/routes/opt-model/plane/assignmentPlane.routes.js
@@ -58,7 +58,7 @@ router.put('/:id', updateAssignmentPlane);
  * /assignmentPlanes/:id:
  *   delete:
  *     summary: Elimina una asignación de avión existente
- *     tags:https://github.com/renatoqueirolo/proyecto-mpm/pull/39/conflict?name=src%252Froutes.js&ancestor_oid=2d051d77b214798636a1205d147696bdf128b773&base_oid=ccddb71163a83229c9921f3d024668e58e782dce&head_oid=5626bdd9ce62ef3f51ba1a5a07359fb6a3f3fad6
+ *     tags:
  *      - Asignaciones de Aviones
  *     responses:
  *       204:
