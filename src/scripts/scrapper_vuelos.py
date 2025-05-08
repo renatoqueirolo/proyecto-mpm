4909c752d5285d42606a6fde6e03f064146aa026
Sebastian Lopez <sebalopeza@uc.cl>
2025-05-08 00:30:53 -0400

    find build id in request

--- src/scripts/scrapper_vuelos.py
+++ src/scripts/scrapper_vuelos.py
@@ -3,6 +3,7 @@ import requests
 import json
 # from pathlib import Path
 import uuid
+import re
 
 def obtener_vuelos_sky(origen: str, destino: str, fecha: str):
     url = "https://api.skyairline.com/farequoting/v1/search/flight?stage=IS"
@@ -82,9 +83,41 @@ def obtener_vuelos_sky(origen: str, destino: str, fecha: str):
 
     return vuelos
 
+def obtener_build_id_latam(origen: str, destino: str, fecha: str):
+    url = "https://www.latamairlines.com/es-cl/flights/"
+    headers = {
+        "Accept": "*/*",
+        "Accept-Language": "es-419,es;q=0.9,es-ES;q=0.8,en;q=0.7,en-GB;q=0.6,en-US;q=0.5,it;q=0.4,es-CL;q=0.3",
+        "Priority": "u=1, i",
+        "Referer": f"https://www.latamairlines.com/cl/es/ofertas-vuelos?origin={origen}&outbound={fecha}T00%3A00%3A00.000Z&destination={destino}&inbound=null&adt=1&chd=0&inf=0&trip=OW&cabin=Economy&redemption=false&sort=RECOMMENDED",
+        "Sec-CH-UA": '"Microsoft Edge";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        "Sec-CH-UA-Mobile": "?0",
+        "Sec-CH-UA-Platform": '"Windows"',
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0"
+    }
+
+    response = requests.get(url, headers=headers, timeout=30)
+
+    if response.status_code != 200:
+        raise Exception(f"Error cargando página de LATAM: {response.status_code}")
+
+    html = response.text
+
+    # Buscar el build_id
+    match = re.search(r'_next/static/([a-zA-Z0-9]+)/_buildManifest.js', html)
+    if match:
+        build_id = match.group(1)
+        return build_id
+    else:
+        raise Exception("No se pudo encontrar el build_id automáticamente.")
+
+
 
 def obtener_search_token(origen: str, destino: str, fecha: str):
-    build_id = "peUUmlN5MeV8r74FC9eoe"  # Este es el que encontraste; si cambia hay que actualizarlo manualmente
+    build_id = obtener_build_id_latam(origen, destino, fecha)  
 
     url = f"https://www.latamairlines.com/es-cl/flights/_next/data/{build_id}/es-cl/flights.json"
     params = {
